8df8f1ad3965fe47f85b0b1e9bba40162dc36614
shijie <shijiechn@aliyun.com>
2022-11-04 23:22:02 +0800

    多选框

--- src/components/SivCheckbox/index.jsx
+++ src/components/SivCheckbox/index.jsx
@@ -1,14 +1,25 @@
 import './index.scss'
 
+/**
+ * 多选框
+ * @param {*} children 子元素 
+ * @param {Function} onChange 多选框回调选中与取消  
+ * @param {Boolean} checked  控制多选框选中状态
+ * @returns 
+ */
 function SivCheckbox({
   children,
   onChange,
   checked
 }) {
 
+  /**
+   * 功能：控制子元素显示。
+   * children没有就不显示，返回空字符串。
+   * children存在则使用span包裹后返回
+   * @returns 需要渲染的子元素
+   */
 
-  // children没有就不显示，返回空字符串。
-  // children存在则使用span包裹后返回
   const createText = () => {
     if (typeof children !== 'string') {
       return ''
