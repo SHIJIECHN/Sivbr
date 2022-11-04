9e4c7b1e811e2f64ce4f19954df9be2866b098fd
shijie <shijiechn@aliyun.com>
2022-11-04 23:22:30 +0800

    列表多选框

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import SivCheckList from "./components/SivCheckboxList/index.jsx";
+import SivButton from './components/SivButton'
+import SivCheckbox from './components/SivCheckbox'
 import studentData from "./mock/student.js";
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 
 function App() {
+  /**
+   * 功能：按钮点击回调响应
+   */
+  const btnClcik = () => {
+    console.log('btn click')
+  }
+
+  /**
+   * 功能：多选框回调响应
+   */
+  const [checked, setChecked] = useState(false);
+  const checkboxChange = (e) => {
+    setChecked(e.target.checked);
+    console.log('checked')
+  }
+
   // 缓存函数，useCallback返回一个记忆回调函数
+  /**
+   * 功能：获取选中的数组项
+   */
   const getCheckedData = useCallback((data) => {
     console.log(data);
   })
 
   return (
     <div>
+      <SivButton
+        type="warn"
+        onClick={btnClcik}
+      >
+        Click
+      </SivButton>
+      <hr />
+      <SivCheckbox
+        checked={checked}
+        onChange={checkboxChange}
+      >
+        This is checkbox
+      </SivCheckbox>
+      <hr />
       <SivCheckList
         data={studentData}
         headerCells={
--- src/components/SivCheckboxList/index.jsx
+++ src/components/SivCheckboxList/index.jsx
@@ -1,8 +1,18 @@
 import './index.scss'
 import { useState } from 'react'
 import SivCheckbox from '../SivCheckbox/index.jsx';
-import { useCallback, useMemo } from 'react';
+import SivButton from '../SivButton/index.jsx'
+import { useCallback, useMemo, useEffect } from 'react';
 
+/**
+ * 列表多选框
+ * @param {Array} data 列表数据数组
+ * @param {Array} headerCells 列表头部
+ * @param {Object} allCheckedTip 头部多选框文字展示
+ * @param {Function} onCheckedDataChange 选中或取消选中数据项时，
+ *        返回被选中的数组项
+ * @returns 
+ */
 function SivCheckList({
   data,
   headerCells,
@@ -17,13 +27,6 @@ function SivCheckList({
   const dataProps = Object.keys(originData[0]);
 
   /**
-   * 功能：点击全选
-   */
-  const setAllCheck = useCallback((e) => {
-    console.log(e);
-  })
-
-  /**
    * 功能：计算到底应该显示allCheckedTip的none还是all
    * useMemo很多情况可以当做vue中的计算熟悉来用，它里面有个逻辑导致重新计算
    * useMemo依赖改变需要重新计算
@@ -35,14 +38,55 @@ function SivCheckList({
     } else {
       return allCheckedTip.none || 'Cancel all'
     }
+  }, [checkedData]);
+
+
+  /**
+   * 当checkedData有改变的时候执行onCheckedDataChange
+   */
+  useEffect(() => {
+    onCheckedDataChange(checkedData);
   }, [checkedData])
+
+  /**
+   * 功能：点击全选。
+   * 点击全选如果e.target.checked为true，那么originData就全部放进
+   * checkedData里；如果假说明反选，也就是撤销掉了，就把checkedData清空
+   */
+  const setAllCheck = useCallback((e) => {
+    const _checked = e.target.checked;
+    setCheckedData(_checked ? originData : [])
+  })
+
+  /**
+   * 功能：点击每一项的多选框。
+   * 如果e.target.checked为true，则往checkedData放入item，如果是假
+   * 则把当前项从checkedData里删除掉
+   */
+  const setSingleCheck = useCallback((e, item) => {
+    const _checked = e.target.checked;
+    setCheckedData(checkedData => _checked ?
+      [...checkedData, item] : // 合并item和checkedData
+      checkedData.filter(data => data.id !== item.id)
+
+    )
+  })
+
+  /**
+   * 功能：删除项。
+   * 同时从originData和checkedData中删除
+   */
+  const removeItem = useCallback((id) => {
+    setOriginData(originData => originData.filter(data => data.id !== id));
+    setCheckedData(checkedData => checkedData.filter(data => data.id !== id))
+  })
 
   return (
     <div>
       <table border="1" width="500" align="center">
         <thead>
           <tr>
-            <td colSpan={5}>
+            <td colSpan={5} align="left">
               <SivCheckbox
                 checked={checkedData.length === originData.length}
                 onChange={(e) => setAllCheck(e)}
@@ -51,7 +95,46 @@ function SivCheckList({
               </SivCheckbox>
             </td>
           </tr>
+          {
+            headerCells && (
+              <tr>
+                {
+                  headerCells.map(item => (
+                    <th key={item}>{item}</th>
+                  ))
+                }
+              </tr>
+            )
+          }
         </thead>
+        <tbody>
+          {/* 渲染data  */}
+          {
+            originData.length !== 0 && originData.map(item => (
+              <tr key={item.id}>
+                <td>
+                  <SivCheckbox
+                    checked={
+                      checkedData.some(data => data.id === item.id)
+                    }
+                    onChange={(e) => setSingleCheck(e, item)}
+                  ></SivCheckbox>
+                </td>
+                {
+                  dataProps.map(prop => (
+                    <td key={prop}>{item[prop]}</td>
+                  ))
+                }
+                <td>
+                  <SivButton
+                    type="warn"
+                    onClick={() => removeItem(item.id)}
+                  >删除</SivButton>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     </div>
   )
